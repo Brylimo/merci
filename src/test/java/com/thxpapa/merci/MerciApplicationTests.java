1d9ce89ae100015f75cf00c9e59eb46783114ddc
brylimo <icj0103@gmail.com>
2023-08-15 18:02:33 +0900

    Initial commit

---
+++ src/test/java/com/thxpapa/merci/MerciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thxpapa.merci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
