91a766046dc6142951d81a5285fdd531a059029e
chaejin <icj0103@gmail.com>
2023-08-16 17:42:13 +0900

    application-dev.properties 생성

--- src/main/java/com/thxpapa/merci/MerciApplication.java
+++ src/main/java/com/thxpapa/merci/MerciApplication.java
@@ -1,13 +1,22 @@
 package com.thxpapa.merci;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MerciApplication {
+public class MerciApplication extends SpringBootServletInitializer {
+	static String APPLICATION_DEV = "spring.config.location=classpath:/application-dev.properties";
 
 	public static void main(String[] args) {
-		SpringApplication.run(MerciApplication.class, args);
+		// new SpringApplicationBuilder(MerciApplication.class).properties(APPLICATION_DEV).run(args);
+		new SpringApplicationBuilder(MerciApplication.class).run(args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MerciApplication.class);
 	}
 
 }
