1966db575aa336cc94a20aec97df8a7e617fa4eb
brylimo <icj0103@gmail.com>
2023-09-19 22:25:28 +0900

    로그아웃 기능 구현

---
+++ src/main/java/com/thxpapa/merci/web/AuthController.java
@@ -0,0 +1,30 @@
+package com.thxpapa.merci.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(value = "/auth")
+public class AuthController {
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest req, HttpServletResponse res) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(req, res, auth);
+        }
+
+        return "redirect:/login";
+    }
+}
