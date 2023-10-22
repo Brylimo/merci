03bb7c7ea271436fecd002be45085c5caedc68a5
brylimo <icj0103@gmail.com>
2023-10-22 23:57:17 +0900

    블로그 커서 이동 개발중

---
+++ src/main/resources/static/js/plaza/class/BlogUtil.js
@@ -0,0 +1,31 @@
+class BlogUtil {
+    static timers = [];
+    static isActive = false;
+
+    static timerFn() {
+        const target = $(".wp .cursors");
+
+        if (target.hasClass("cursor-visible")) {
+            target.removeClass("cursor-visible");
+        } else {
+            target.addClass("cursor-visible");
+        }
+    }
+
+    static timerPlayer() {
+        let timerId = setTimeout(() => {
+            BlogUtil.timerFn();
+
+            BlogUtil.timers.splice(0);
+            BlogUtil.timerPlayer();
+        }, 450)
+
+        BlogUtil.timers.push(timerId);
+    }
+
+    static clearTimerPlayer() {
+        for (let i = 0; i < BlogUtil.timers.length; i++) {
+            clearTimeout(BlogUtil.timers[i]);
+        }
+    }
+}
--- src/main/resources/static/js/plaza/publish.js
+++ src/main/resources/static/js/plaza/publish.js
@@ -1,6 +1,7 @@
 $(() => {
     window.addEventListener('resize', function () {
-        console.log("zpzp");
+        const workplaceWidth = document.querySelector(".workplace-wrapper").offsetWidth;
+        document.querySelector(".publish-footer").style.width = workplaceWidth + 'px';
     })
 
     $(".hashtag input[name='hashtag-txt']").on("keyup", function(event){
@@ -15,9 +16,68 @@ $(() => {
 
     $(".workplace-wrapper .title input[name='title']").on("keyup", function (event) {
         $(".preview-wrapper .title span").text(event.target.value);
+    });
+
+    $(".publish-footer .out-btn").click(() => {
+        window.history.back();
+    });
+
+    $(document).on("keydown", (event) => {
+        if (BlogUtil.isActive) {
+            const $textarea = $(".wp-textarea-wrapper textarea");
+            const $targetSpan = $(".wp-code .wp-line span");
+
+            if (event.keyCode === 13) { // enter
+                const $wpCodeLine = $("<pre class='wp-line'><span></span></pre>");
+                $(".wp-code").append($wpCodeLine);
+            } else if (event.keyCode === 37) { // left arrow
+
+            } else if (event.keyCode === 39) { // right arrow
+
+            } else if (event.keyCode === 38) { // up arrow
+
+            } else if (event.keyCode === 40) { // down arrow
+
+            } else {
+                $textarea.focus();
+            }
+        }
+    });
+
+    $(".wp-textarea-wrapper textarea").on("keyup", (event) => {
+        const $textarea = $(".wp-textarea-wrapper textarea");
+        const $targetSpan = $(".wp-code .wp-line span");
+        const $wpWrapper = $(".wp-textarea-wrapper");
+        const $cursor = $(".wp-core .cursors .cursor")
+
+        if (event.keyCode === 37) {
+            $targetSpan.val().slice(0, -1);
+        } else {
+            $targetSpan.text($textarea.val());
+        }
+
+        $wpWrapper.css("left", $targetSpan.width() + "px");
+        $cursor.css("left", $targetSpan.width() + "px");
+    });
+
+    $(".wp-textarea-wrapper textarea").on("blur", function (event) {
+        BlogUtil.clearTimerPlayer();
+        $(".wp .cursors").removeClass("cursor-visible");
+        BlogUtil.isActive = false;
+    });
+
+    $(".wp").on("click", (event) => {
+        BlogUtil.timerPlayer();
+        BlogUtil.isActive = true;
     })
 
-    $(".workplace-wrapper .workplace .wp textarea").on("keyup", function (event) {
-        $(".preview-wrapper .workplace span").text(event.target.value);
-    })
+    init();
 })
+
+const init = () => {
+    const workplaceWidth = document.querySelector(".workplace-wrapper").offsetWidth;
+    document.querySelector(".publish-footer").style.width = workplaceWidth + 'px';
+
+    const $wpWrapper = $(".wp-textarea-wrapper");
+    $wpWrapper.css("top", $(".wp-textarea-wrapper").height() + "px");
+}
